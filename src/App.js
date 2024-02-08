3172f2c7f5a303f681b704d4a4fd3ece701bc08b
racerf01 <racerf01@mail.ru>
2024-02-08 19:18:37 +0100

    lesson 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React from "react";
+import RecipeList from "./RecipeList";
+
 function App() {
   return (
-    null
+    <RecipeList recipes = {sampleRecipes}/>
   );
 }
-
+ const sampleRecipes = [
+  {
+    id: 1, 
+    name: 'Plain Chicken',
+    servings: 3, 
+    cookTime: '1.45', 
+    instructions: "1. Put salt on chicken\n2. Put chicken in the oven\n3. Eat the chicken"
+  }, 
+  {
+    id: 1, 
+    name: 'Plain Pork',
+    servings: 5, 
+    cookTime: '0.45', 
+    instructions: "1. Put paprika on pork\n2. Put pork in the oven\n3. Eat the pork"
+  }
+ ]
 export default App;
