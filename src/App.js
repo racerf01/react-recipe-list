4ce17162a9ff35813b496d3f129a4dbd6080926f
racerf01 <racerf01@mail.ru>
2024-02-09 17:48:36 +0100

    lesson1.1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import RecipeList from "./RecipeList";
+import Ingredient from "./Ingredient";
 
 function App() {
   return (
@@ -12,14 +13,40 @@ function App() {
     name: 'Plain Chicken',
     servings: 3, 
     cookTime: '1.45', 
-    instructions: "1. Put salt on chicken\n2. Put chicken in the oven\n3. Eat the chicken"
+    instructions: "1. Put salt on chicken\n2. Put chicken in the oven\n3. Eat the chicken", 
+    ingredients: [
+    {
+      id: 1,
+      name: 'Chicken',
+      amount: '2 Pounds'
+    }, 
+    {
+      id: 2,
+      name: 'Salt',
+      amount: '1 Tbs'
+    }, 
+    
+
+    ]
   }, 
   {
     id: 1, 
     name: 'Plain Pork',
     servings: 5, 
     cookTime: '0.45', 
-    instructions: "1. Put paprika on pork\n2. Put pork in the oven\n3. Eat the pork"
+    instructions: "1. Put paprika on pork\n2. Put pork in the oven\n3. Eat the pork",
+    ingredients: [
+      {
+        id: 1,
+        name: 'Pork',
+        amount: '3 Pounds'
+      }, 
+      {
+        id: 2,
+        name: 'Paprika',
+        amount: '2 Tbs'
+      }, 
+    ]
   }
  ]
 export default App;
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import IngredientList from './IngredientList'
+
 
 export default function Recipe(props) {
     const {
         name,
         cookTime,
         servings, 
-        instructions
+        instructions,
+        ingredients
     } = props
     return (
         <div>
@@ -28,6 +31,12 @@ export default function Recipe(props) {
             <span>Instructions</span>
             <div>{instructions}</div>
         </div>
+        <div>
+            <span>Ingredients</span>
+            <div>
+            <IngredientList ingredients={ingredients} />
+            </div>
         </div>
+    </div>
     )
     }
