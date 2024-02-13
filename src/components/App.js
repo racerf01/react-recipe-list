80c731c273152185f25beafa27852c04f15179a3
racerf01 <racerf01@mail.ru>
2024-02-13 20:34:03 +0100

    Lesson4

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import RecipeList from "./RecipeList";
 import '../css/app.css'
-import uuid from 'uuid'
+import { v4 as uuidv4 } from 'uuid';
+import ingredientList from "./IngredientList";
 
 function App() {
+
+  const [recipes, setRecipes] = useState(sampleRecipes)
+
+  function handleRecipeAdd() {
+    const newRecipe = {
+      id: uuidv4(), 
+      name: 'New',
+      servings: 1, 
+      cookTime: '1:00',
+      instructions: 'Instr.',
+      ingredients: [
+        {id: uuidv4(), name: 'Name', amount: '1 Tbs'}
+      ]
+    }
+  
+    setRecipes([...recipes, newRecipe])
+  }
+
+  function handleRecipeDelete(id) {
+    setRecipes(recipes.filter(recipes => recipes.id !== id))
+  }
+
+
   return (
-    <RecipeList recipes = {sampleRecipes}/>
+    <RecipeList 
+      recipes = {recipes}
+      handleRecipeAdd={handleRecipeAdd}
+      handleRecipeDelete={handleRecipeDelete}
+    />
   );
 }
+
+
+
+
  const sampleRecipes = [
   {
     id: 1, 
